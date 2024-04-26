27b4417888bfc1488cb62f2a2b3c000a2ee05170
Wei-Hsiang86 <frankflyhuang@gmail.com>
2024-04-26 19:49:01 +0800

    feat: add admin index page

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
+const admin = require('./modules/admin')
+
 const restController = require('../controllers/restaurant-controller')
 
 // 寫路由要注意，條件複雜的判斷網上放，簡單的往下擺
-router.get('/restaurants', restController.getRestaurants)
+router.use('/admin', admin)
 
+router.get('/restaurants', restController.getRestaurants)
 // 設定 fallback 路由，意義為其他路由條件都不符合時，最終會通過的路由
 // 也就是說，當程式一路由上而下執行，萬一都匹配不到和請求相符的路徑，此時不論此 request 是用哪個 HTTP method 發出的，都會匹配到這一行
 // 注意這裡的 method 是用 use
