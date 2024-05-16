1468fc08d12735fcc82709609889a89c29fc8b45
Wei-Hsiang86 <frankflyhuang@gmail.com>
2024-05-16 15:42:34 +0800

    feat: user model add is_admin

--- helpers/auth-helpers.js
+++ helpers/auth-helpers.js
@@ -2,6 +2,12 @@
 const getUser = req => {
   return req.user || null
 }
+
+const ensureAuthenticated = req => {
+  return req.isAuthenticated()
+}
+
 module.exports = {
-  getUser
+  getUser,
+  ensureAuthenticated
 }
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ const admin = require('./modules/admin')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 
+const { authenticated } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
 // 寫路由要注意，條件複雜的判斷網上放，簡單的往下擺
@@ -22,7 +23,7 @@ router.post('/signin', passport.authenticate('local', { failureRedirect: '/signi
 
 router.get('/logout', userController.logout)
 
-router.get('/restaurants', restController.getRestaurants)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 
 // 設定 fallback 路由，意義為其他路由條件都不符合時，最終會通過的路由
 // 也就是說，當程式一路由上而下執行，萬一都匹配不到和請求相符的路徑，此時不論此 request 是用哪個 HTTP method 發出的，都會匹配到這一行
