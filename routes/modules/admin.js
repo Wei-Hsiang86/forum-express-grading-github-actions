6110b30fa042d1b08e92624d5ad043992885d4d6
Wei-Hsiang86 <frankflyhuang@gmail.com>
2024-05-16 19:34:38 +0800

    feat: admin restaurant page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,9 +3,13 @@ const router = express.Router()
 
 const adminController = require('../../controllers/admin-controller')
 
+// 路由的判斷順序很重要，其邏輯是找對相對應的路徑就會 ,mapping 進去，後續就直接忽略
+// 所以會把相對嚴格的判斷放在外面
 router.get('/restaurants/create', adminController.createRestaurant)
+router.get('/restaurants/:id', adminController.getRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', adminController.postRestaurant)
+
 router.use('/', (req, res) => { res.redirect('/admin/restaurants') })
 
 module.exports = router
