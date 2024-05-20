e2229122aead415841df9ac1bdab5b5a200438a1
Wei-Hsiang86 <frankflyhuang@gmail.com>
2024-05-21 02:02:18 +0800

    feat: add category model

--- migrations/20240516065255-add-is-admin-to-users.js
+++ migrations/20240516065255-add-is-admin-to-users.js
@@ -3,6 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    // 對於直接操作資料庫的名字，就用資料庫的命名法 (snack case)
     await queryInterface.addColumn('Users', 'is_admin', {
       type: Sequelize.BOOLEAN,
       defaultValue: false
