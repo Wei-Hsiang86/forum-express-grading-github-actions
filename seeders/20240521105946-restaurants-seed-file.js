1a85eea58f92cc443781f75a7b072f9a1d7ccec1
Wei-Hsiang86 <frankflyhuang@gmail.com>
2024-05-21 19:05:33 +0800

    feat: update seed files

--- seeders/20240521105946-restaurants-seed-file.js
+++ seeders/20240521105946-restaurants-seed-file.js
@@ -4,16 +4,21 @@ const faker = require('faker')
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const categories = await queryInterface.sequelize.query(
+      'SELECT id FROM Categories;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
     await queryInterface.bulkInsert('Restaurants',
       Array.from({ length: 50 }, () => ({
         name: faker.name.findName(),
         tel: faker.phone.phoneNumber(),
         address: faker.address.streetAddress(),
         opening_hours: '08:00',
-        image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`, // 引用外部服務
+        image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
         description: faker.lorem.text(),
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
+        category_id: categories[Math.floor(Math.random() * categories.length)].id // 取出對應的 categories 陣列的資料
       }))
     )
   },
