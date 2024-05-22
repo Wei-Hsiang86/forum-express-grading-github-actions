c7f12e5f5ddf52d160124a193998e2ca39a786de
Wei-Hsiang86 <frankflyhuang@gmail.com>
2024-05-23 03:26:49 +0800

    feat: add ifCond hbs helper & update category selector

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,5 +1,10 @@
 const dayjs = require('dayjs')
 
 module.exports = {
-  currentYear: () => dayjs().year()
+  currentYear: () => dayjs().year(),
+
+  // 注意這裡不能用箭頭函式，因為箭頭函式的 this 會指到外層的那個 this (module.exports 外的那層)
+  ifCond: function (a, b, options) {
+    return a === b ? options.fn(this) : options.inverse(this)
+  }
 }
