522a2661cac2b00f0d3f83cf66e03cae5b654da3
Wei-Hsiang86 <frankflyhuang@gmail.com>
2024-05-26 22:06:36 +0800

    feat: add pagination on restaurants & admin

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,11 +1,17 @@
 const { Restaurant, Category } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const restaurantController = {
   getRestaurants: (req, res, next) => {
     // 可以注意的是，因為從網址列 query 下來的東西是字串，所以要先轉換成數字
     const categoryId = Number(req.query.categoryId) || ''
 
-    console.log('seeeeeeeee', categoryId)
+    // 分頁器設定
+    const DEFAULT_LIMIT = 9
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
+
     /*
     這裡的 where 可以用非 sequelize 語法來完成
     const where = {}
@@ -21,7 +27,7 @@ const restaurantController = {
     */
 
     return Promise.all([
-      Restaurant.findAll({
+      Restaurant.findAndCountAll({
         include: Category,
         where: { // 查詢條件
           ...categoryId ? { categoryId } : {}
@@ -29,13 +35,15 @@ const restaurantController = {
           // 另外，展開運算子的優先值較低，所以會先執行中間的三元運算子
           // 可以看做醬子 ...(categoryId ? { categoryId } : {})
         },
+        limit,
+        offset,
         nest: true,
         raw: true
       }),
       Category.findAll({ raw: true })
     ])
       .then(([restaurants, categories]) => {
-        const data = restaurants.map(r => ({
+        const data = restaurants.rows.map(r => ({
           ...r,
 
           // 使前端拿到的資料縮減
@@ -44,7 +52,8 @@ const restaurantController = {
         return res.render('restaurants', {
           restaurants: data,
           categories,
-          categoryId
+          categoryId,
+          pagination: getPagination(limit, page, restaurants.count)
         })
       })
       .catch(err => next(err))
