41eee16a3a8258903dddd8b7219e46323f5a76f7
Wei-Hsiang86 <frankflyhuang@gmail.com>
2024-07-08 21:37:12 +0800

    feat: add api error handler & delete restaurant api

--- controllers/apis/admin-controller.js
+++ controllers/apis/admin-controller.js
@@ -2,7 +2,11 @@ const adminServices = require('../../services/admin-services')
 
 const adminController = {
   getRestaurants: (req, res, next) => {
-    adminServices.getRestaurants(req, (err, data) => err ? next(err) : res.json(data))
+    adminServices.getRestaurants(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    // 透過新增 status 使前端做判斷的時候可以更好的撰寫處理方式
+  },
+  deleteRestaurant: (req, res, next) => {
+    adminServices.deleteRestaurant(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   }
 }
 
--- controllers/apis/restaurant-controller.js
+++ controllers/apis/restaurant-controller.js
@@ -6,7 +6,7 @@ const restaurantController = {
   getRestaurants: (req, res, next) => {
     // 這裡的 (err, data)，對應到的是 getRestaurants 第二個參數，也就是 cb 函式
     // 代表的是呼叫一個 callback 函式
-    restaurantServices.getRestaurants(req, (err, data) => err ? next(err) : res.json(data))
+    restaurantServices.getRestaurants(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   }
 }
 module.exports = restaurantController
